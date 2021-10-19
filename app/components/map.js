378b1d408cdce088dff21315e575afa6c1d5c9ad
Vlad Bicu <vladbicu@users.noreply.github.com>
2021-10-19 16:39:44 +0300

    Improvements

--- app/components/map.js
+++ app/components/map.js
@@ -1,3 +1,18 @@
 import Component from '@glimmer/component';
+import ENV from 'super-rentals/config/environment';
 
-export default class MapComponent extends Component {}
+const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
+
+export default class MapComponent extends Component {
+  get src() {
+    const { lng, lat, width, height, zoom } = this.args;
+    const coordinates = `${lng},${lat},${zoom}`;
+    const dimensions = `${width}x${height}`;
+    const accessToken = `access_token=${this.token}`;
+
+    return `${MAPBOX_API}/${coordinates}/${dimensions}@2x?${accessToken}`;
+  }
+  get token() {
+    return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
+  }
+}
--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -6,21 +6,35 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | rental', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders information about a rental property', async function (assert) {
+    this.setProperties({
+      rental: {
+        title: 'Grand Old Mansion',
+        owner: 'Veruca Salt',
+        city: 'San Francisco',
+        location: {
+          lat: 37.7749,
+          lng: -122.4194,
+        },
+        category: 'Estate',
+        type: 'Standalone',
+        bedrooms: 15,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+        description:
+          'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
+      },
+    });
 
-    await render(hbs`<Rental />`);
+    await render(hbs`<Rental @rental={{this.rental}} />`);
 
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Rental>
-        template block text
-      </Rental>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('article').hasClass('rental');
+    assert.dom('article h3').hasText('Grand Old Mansion');
+    assert.dom('article .detail.owner').includesText('Veruca Salt');
+    assert.dom('article .detail.type').includesText('Standalone');
+    assert.dom('article .detail.location').includesText('San Francisco');
+    assert.dom('article .detail.bedrooms').includesText('15');
+    assert.dom('article .image').exists();
+    assert.dom('article .map').exists();
   });
 });
